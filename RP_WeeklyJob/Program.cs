c1c46567164d60a7b81f4cdcbc84a39aa09d32aa
tloy1966 <tloy1966@gmail.com>
2016-09-15 05:30:58 +0800

    Add new project for week job

---
+++ RP_WeeklyJob/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP_WeeklyJob
+{
+    class Program
+    {
+        /// <summary>
+        /// Get data from API
+        /// Regulation : 2 week
+        /// API info : http://plvr.land.moi.gov.tw/DownloadOpenData
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
